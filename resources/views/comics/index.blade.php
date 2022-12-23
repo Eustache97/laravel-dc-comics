5741d73cc9b0c1fef02bb49da533cc0182b4cf6e
Eustache97 <eubomber97@gmail.com>
2022-12-23 02:24:35 +0100

    aggiunta visualizzazione singolo comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,12 +2,13 @@
 @section('content')
     <div class="container">
         <h2>Lista dei comics</h2>
-        <table class="table table-striped">
+        <table class="table table-dark table-striped">
             <thead>
                 <tr>
                     <th scope="col">Titolo</th>
                     <th scope="col">Serie</th>
                     <th scope="col">Tipologia</th>
+                    <th scope="col">Dettagli</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,6 +17,9 @@
                         <th scope="row">{{ $comic->title }}</th>
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->type }}</td>
+                        <td>
+                            <a class="btn btn-success" href="{{ route('comics.show', $comic->id) }}">Vedi</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
